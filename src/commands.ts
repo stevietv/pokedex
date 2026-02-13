1782d9d384eb8098b05ba17cbdf039e5a1d1caea
StevieTV <stevie.robinson@gmail.com>
2026-02-13 16:26:50 +0100

    restructure commands

---
+++ src/commands.ts
@@ -0,0 +1,19 @@
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+import { CLICommand } from "./command.js";
+
+
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        help: {
+            name: "help",
+            description: "lists all commands",
+            callback: commandHelp,
+        },
+        exit: {
+            name: "exit",
+            description: "exits the pokedex",
+            callback: commandExit,
+        },
+    };
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,22 +1,39 @@
 import { createInterface } from "node:readline";
-import { commandExit } from "./command_exit.js";
-import { commandHelp } from "./command_help.js";
-import { CLICommand } from "./command.js";
+import { getCommands } from "./commands.js";
 
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
 
-export function getCommands(): Record<string, CLICommand> {
-    return {
-        exit: {
-            name: "exit",
-            description: "exits the pokedex",
-            callback: commandExit,
-        },
-        help: {
-            name: "help",
-            description: "lists all commands",
-            callback: commandHelp,
-        },
-    };
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        const inputArray = cleanInput(input);
+        if (inputArray.length === 0) {
+            rl.prompt();
+            return;
+        }
+
+        const command = getCommands()[inputArray[0]];
+
+        if (command) {
+            try {
+                command.callback(getCommands());
+            } catch (err) {
+                if (err instanceof Error) {
+                    console.error("Error encounterd: ", err);
+                }
+            }
+        } else {
+            console.log("Unknown command. Type 'help' for a list of commands.");
+        }
+        
+        rl.prompt();
+      }
+    )
 }
 
 
@@ -27,35 +44,4 @@ export function cleanInput(input: string): string[] {
     .split(/\s+/);
     output = output.filter((item) => item.length > 0);
     return output;
-}
-
-export function startREPL() {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex > ",
-    });
-    rl.prompt();
-    rl.on("line", (input) => {
-        const inputArray = cleanInput(input);
-        if (inputArray.length === 0) {
-            rl.prompt();
-            return;
-        }
-        const command = getCommands()[inputArray[0]];
-        if (command) {
-            try {
-                command.callback(getCommands());
-            } catch (err) {
-                if (err instanceof Error) {
-                    console.error("Error encounterd: ", err);
-                }
-            }
-        } else {
-            console.log("Unknown command");
-        }
-        
-        rl.prompt();
-      }
-    )
 }
